9ea4f0b49897585b5ba1973b05f50597e6d39fe6
kos <kkos44911@gmail.com>
2020-12-03 22:45:42 +0300

    Add project files.

---
+++ Notification/NotificationForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Notification
+{
+    partial class NotificationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotificationForm));
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.headerLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.headerLabel.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.Location = new System.Drawing.Point(-1, 0);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.headerLabel.Size = new System.Drawing.Size(421, 21);
+            this.headerLabel.TabIndex = 0;
+            this.headerLabel.Text = "Новое оповещение";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(386, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 18);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Yandex-UI-Icons-Private", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(417, 60);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Тут много текста оповещения";
+            // 
+            // NotificationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(420, 95);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.headerLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "NotificationForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.NotificationForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label headerLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Notification/NotificationForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Notification
+{
+    public partial class NotificationForm : Form
+    {
+
+        private Point FormStartPosition = new Point();
+        private Size FormSize = new Size();
+
+        private Point Gap = new Point(5, 5);
+        private int TimeToHide = 8;//todo
+
+        public NotificationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void NotificationForm_Load(object sender, EventArgs e)
+        {
+            FormSize = this.Size;
+            // Setup showing location
+            var UserResolution = Screen.PrimaryScreen.WorkingArea;
+            SetupViewLocation(UserResolution.Width - FormSize.Width - Gap.X, UserResolution.Height - FormSize.Height - Gap.Y);
+            this.Location = FormStartPosition;
+        }
+
+        public void SetupViewLocation(int x, int y)
+        {
+            int CurrentResolutionHeight = Screen.PrimaryScreen.WorkingArea.Height;
+            int CurrentResolutionWidth = Screen.PrimaryScreen.WorkingArea.Width;
+
+            if (x < 0 || y < 0)
+                throw new Exception("Location cannot be lower than Zero.");
+            else if (y > CurrentResolutionHeight || x > CurrentResolutionWidth)
+                throw new Exception("x or y cannot be bigger than current user resolution");
+            FormStartPosition = new Point(x,y);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+}
+
+// TODO
+// Change size of form & controls to user resolution
